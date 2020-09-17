3a4396d1b8ead5c303640e8775567ff0ded3815d
GW <364199574@qq.com>
2020-09-17 17:00:34 +0800

    add Student

---
+++ src/main/java/com/cetc/entity/Student.java
@@ -0,0 +1,10 @@
+package com.cetc.entity;
+
+import java.util.PrimitiveIterator;
+
+public class Student {
+    private String id;
+    private String name;
+    private String gender;
+    private Integer age;
+}
